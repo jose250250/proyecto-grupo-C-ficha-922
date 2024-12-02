4140867099c659081f0f6a2e83e31fe00bbf901c
jose250250 <jose_apena@soy.sena.edu.co>
2024-12-02 18:19:23 -0500

    Add files via upload
    se añade el backen

---
+++ backend/gestion-turistica/src/main/java/co/edu/sena/gestion_turistica/GestionTuristicaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.gestion_turistica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTuristicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTuristicaApplication.class, args);
+	}
+
+}
---
+++ backend/gestion-turistica/src/test/java/co/edu/sena/gestion_turistica/GestionTuristicaApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.sena.gestion_turistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionTuristicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
