a0d50a15d10556c2748ec62b8ae6128a8ad14468
jose angel pena <jose_apena@soy.sena.edu.co>
2024-12-18 09:09:13 -0500

    se actualiza el backend con maven

---
+++ backend/gestionTuristica/src/main/java/co/edu/sena/gestionTuristica/GestionTuristicaApplication.java
@@ -0,0 +1,13 @@
+package co.edu.sena.gestionTuristica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionTuristicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionTuristicaApplication.class, args);
+	}
+
+}
